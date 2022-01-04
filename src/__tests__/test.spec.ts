e9babadf2ee8a2629af0f4f1688dc59e8a8b8efc
WendelSantosD <wendelwcsantos@gmail.com>
2022-01-03 22:34:03 -0300

    implements babel

--- src/__tests__/test.spec.ts
+++ src/__tests__/test.spec.ts
@@ -1,7 +1,7 @@
 describe('Filter function', () => {
-	test('it should filter by a search term (link)', () => {
-		const result = 2 + 2
+    test('it should filter by a search term (link)', () => {
+        const result = 2 + 2
 
-		expect(result).toBe(4)
-	})
+        expect(result).toBe(4)
+    })
 })
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import cors from 'cors'
+import express, { json, urlencoded } from 'express'
+import morgan from 'morgan'
+
+const app = express()
+
+app.use(cors({
+    origin: '*'
+}))
+app.use(json())
+app.use(urlencoded({ extended: true}))
+app.use(morgan('dev'))
+
+app.listen(3333, () => {
+    console.log('server is running')
+})
