1b9f741d89cfb5a8de2d13e9201c9feacbe9dd4f
WendelSantosD <wendelwcsantos@gmail.com>
2022-02-08 23:11:31 -0300

    implements test to create user

--- src/__tests__/integration/user.spec.ts
+++ src/__tests__/integration/user.spec.ts
@@ -12,6 +12,33 @@ describe('Authentication', () => {
             })
         
         expect(response.status).toBe(200)
-      
+    })
+})
+
+describe('Create User', () => {
+    it('should be able to create a user', async () => {
+        const response = await request(app)
+            .post('/user/create')
+            .send({
+                name: 'Wendel Santos',
+                phone: '91984379366',
+                email: 'wendelwcsantos@gmail.com',
+                password: 'wendel'
+            })
+        
+        expect(response.status).toBe(201)
+    })
+
+    it('should not be able to create a user with the same email', async () => {
+        const response = await request(app)
+            .post('/user/create')
+            .send({
+                name: 'Wendel Santos',
+                phone: '91984379366',
+                email: 'wendelwcsantos@gmail.com',
+                password: 'wendel'
+            })
+        
+        expect(response.status).toBe(500)
     })
 })
