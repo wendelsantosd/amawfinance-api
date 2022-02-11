700a2a672535f896e168847154299c7d5921da58
WendelSantosD <wendelwcsantos@gmail.com>
2022-02-11 12:36:11 -0300

    delete file

--- src/controllers/profilePictureController.ts
+++ src/controllers/profilePictureController.ts
@@ -1,14 +1,20 @@
-import { Request, Response } from 'express'
+import aws from 'aws-sdk'
+import { Response } from 'express'
+import fs from 'fs'
+import path from 'path'
+import { promisify } from 'util'
 
 import { prisma } from '../database'
 import { nowLocalDate } from '../provider/nowLocalDate'
 import { CustomPictureProfileRequest, CustomRequest } from '../types'
 
+const s3: any = new aws.S3()
+
 const profilePictureController = {
     create: async (req: CustomPictureProfileRequest, res: Response): Promise<void> => {
         try {
             const { user } = req
-            const { id } = req.body
+            const { id } = req.query
 
             if (id) {
                 if (user?.access_level === 'admin' || user?.id === id) {
@@ -38,21 +44,54 @@ const profilePictureController = {
                                     user_id: targetUser.id
                                 }
                             })
-                        }
 
-                        await prisma.profile_pictures.create({
-                            data: {
-                                name,
-                                size,
-                                id,
-                                url,
-                                user_id: user.id,
-                                created_at: nowLocalDate(),
-                                updated_at: nowLocalDate()
+                            await prisma.profile_pictures.create({
+                                data: {
+                                    name,
+                                    size,
+                                    id,
+                                    url,
+                                    user_id: user.id,
+                                    created_at: nowLocalDate(),
+                                    updated_at: nowLocalDate()
+                                }
+                            })
+                            
+                            res.status(201).json({ message: 'profile picture created'})
+
+                            if (process.env.STORAGE_TYPE === 's3') {
+                                return s3
+                                    .deleteObject({
+                                        Bucket: process.env.BUCKET_NAME,
+                                        Key: id
+                                    })
+                                    .promise()
+                                    .then(response => {
+                                        console.log(response.status)
+                                    })
+                                    .catch(response => {
+                                        console.log(response.status)
+                                    })
+                            } else {
+                                return promisify(fs.unlink)(
+                                    path.resolve(__dirname, '..', '..', 'tmp', 'uploads', existingImage.id)
+                                )
                             }
-                        })
+                        } else {
+                            await prisma.profile_pictures.create({
+                                data: {
+                                    name,
+                                    size,
+                                    id,
+                                    url,
+                                    user_id: user.id,
+                                    created_at: nowLocalDate(),
+                                    updated_at: nowLocalDate()
+                                }
+                            })
 
-                        res.status(201).json({ message: 'profile picture created'})
+                            res.status(201).json({ message: 'profile picture created'})
+                        }
                     } else {
                         res.status(404).json({ message: 'user not found' })
                     }
@@ -60,7 +99,7 @@ const profilePictureController = {
                     res.status(403).json({ message: 'no access' })
                 }
             } else {
-                res.status(412).json({ message: 'missing arguments'})
+                res.status(412).json({ message: 'missing id'})
             }
         } catch (err: any) {
             res.status(500).json({ message: err.message })
