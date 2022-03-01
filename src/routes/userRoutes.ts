3fe34deb50e87b1bf58b18deb4197330efa25363
WendelSantosD <wendelwcsantos@gmail.com>
2022-03-01 15:25:55 -0300

    refactor route modify password

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -19,7 +19,7 @@ userRoutes.delete('/user/delete', verifyToken, userController.delete)
 userRoutes.get('/user/confirm-email/:token', userController.confirmEmail)
 userRoutes.get('/user/recover-password', userController.recoverPassword)
 userRoutes.post('/user/modify-password-by-recover/:token', userController.modifyPasswordByRecover)
-userRoutes.post('/user/modify-password', verifyToken, userController.modifyPassword)
+userRoutes.patch('/user/modify-password', verifyToken, userController.modifyPassword)
 userRoutes.get('/user/modify-email', verifyToken, userController.modifyEmail)
 userRoutes.get('/user/alter-email/:email/:token', userController.alterEmail)
 
