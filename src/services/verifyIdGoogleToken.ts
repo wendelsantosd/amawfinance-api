0dbf5482232387bd867ee10e3acb402a4981e66d
WendelSantosD <wendelwcsantos@gmail.com>
2022-04-16 02:10:45 -0300

    refactor: add google auth to app mobile.

--- src/services/verifyIdGoogleToken.ts
+++ src/services/verifyIdGoogleToken.ts
@@ -1,8 +1,14 @@
 import  { OAuth2Client, TokenPayload } from 'google-auth-library'
 
 
-export const verifyIdGoogleToken  = async (token: string): Promise<TokenPayload | undefined> => {
-    const client_id = '987123219909-tlbchc36o51ahjr96bkbc6rpfekhh4vk.apps.googleusercontent.com'
+export const verifyIdGoogleToken  = async (token: string, isMobile: any): Promise<TokenPayload | undefined> => {
+    console.log(isMobile)
+    const client_id = isMobile ? 
+        '360939615636-9r1cjfm7cktmhchd95gfg65n1bgn2rsu.apps.googleusercontent.com'
+        :
+        '987123219909-tlbchc36o51ahjr96bkbc6rpfekhh4vk.apps.googleusercontent.com'
+    
+    console.log(client_id)
   
     const client = new OAuth2Client(client_id)
   
